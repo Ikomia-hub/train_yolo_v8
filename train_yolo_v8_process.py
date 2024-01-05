00715900b7022d1761e7dc72b0bbb0722e37b849
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2024-01-05 15:41:36 +0100

    Fix: minor version increment

--- train_yolo_v8_process.py
+++ train_yolo_v8_process.py
@@ -203,7 +203,7 @@ class TrainYoloV8Factory(dataprocess.CTaskFactory):
         self.info.short_description = "Train YOLOv8 object detection models."
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Detection"
-        self.info.version = "2.1.2"
+        self.info.version = "2.1.0"
         self.info.icon_path = "icons/icon.png"
         self.info.authors = "Jocher, G., Chaurasia, A., & Qiu, J"
         self.info.article = "YOLO by Ultralytics"
